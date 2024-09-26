e119f392b40b6f28991482c21200e8c6f469feb6
bryndioleksandr <s.v.bryndo@gmail.com>
2024-09-26 13:58:42 +0300

    Finnaly connected Tesseract. Now to-do logic to get coordinates of text on the screenshot

--- AutoClicker/blumkiss/src/logic/Logic.java
+++ AutoClicker/blumkiss/src/logic/Logic.java
@@ -6,11 +6,11 @@ import java.io.File;
 
 public class Logic {
     public static void main(String[] args) {
-        File imageFile = new File("C:\\Users\\sanya\\tess.png"); // Шлях до твого зображення
+        File imageFile = new File("C:\\Users\\sanya\\tess.png");
         Tesseract tesseract = new Tesseract();
 
         try {
-            tesseract.setDatapath("C:\\Users\\sanya\\Downloads\\tess4j-5.13.0.jar\\tessdata"); // Онови до свого шляху tessdata
+            tesseract.setDatapath("C:\\Program Files\\Tesseract-OCR\\tessdata");
             String result = tesseract.doOCR(imageFile);
             System.out.println("Результат OCR: " + result);
         } catch (TesseractException e) {
